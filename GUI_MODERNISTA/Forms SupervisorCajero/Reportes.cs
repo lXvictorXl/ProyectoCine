69883e9a68316da63061cc9e053bd6a5669c2398
ArgotePazRamiro <53713512+ArgotePazRamiro@users.noreply.github.com>
2020-02-01 11:55:00 -0400

    Cambios en el diseño
    reportes del supervisor de cajeros
    
    Co-Authored-By: George <fremosth@users.noreply.github.com>
    Co-Authored-By: argotepazramiro <argotepazramiro@users.noreply.github.com>

--- GUI_MODERNISTA/Forms SupervisorCajero/Reportes.cs
+++ GUI_MODERNISTA/Forms SupervisorCajero/Reportes.cs
@@ -16,5 +16,15 @@ namespace GUI_MODERNISTA
         {
             InitializeComponent();
         }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
